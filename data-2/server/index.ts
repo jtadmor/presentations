ccc8ceb272a0736bee5fe708241cb91146cde839
Yoel Tadmor <jtadmor@gmail.com>
2021-08-25 09:08:29 -0400

    init

---
+++ data-2/server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Up and running');
+});
