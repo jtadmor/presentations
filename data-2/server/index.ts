76f3bcd3fba621a6a94097b8f2475a117e467dbe
Yoel Tadmor <jtadmor@gmail.com>
2021-08-25 16:01:14 -0400

    nodemon working and such

--- data-2/server/index.ts
+++ data-2/server/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import { Pool } from 'pg';
 
 const app = express();
 
 const PORT = 4000;
+
+const pool = new Pool({
+  user: 'tsdbadmin',
+  host: 'w1dt7nu2a0.xeuns2kznv.tsdb.forge.timescale.com',
+  database: 'tsdb',
+  port: 35665,
+  password: 'datatwopointo'
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
@@ -10,4 +19,12 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
   res.send('Up and running');
+});
+
+app.get('/variables', async (req, res) => {
+  const { rows } = await pool.query(`
+    SELECT * from variables
+  `);
+
+  res.status(200).send(JSON.stringify(rows))
 });
