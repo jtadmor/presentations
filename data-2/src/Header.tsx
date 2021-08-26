704c3359c385f680a2a58d1dce3327812d8773cd
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-08-26 14:35:23 +0200

    Basic styling with 2 analyses

---
+++ data-2/src/Header.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  background-color: #2B3950;
+  color: white;
+  text-align: left;
+  padding: 0.8rem;
+`;
+
+export default function Header(){
+  return <StyledHeader>Dashboard / Deffects</StyledHeader>
+};
